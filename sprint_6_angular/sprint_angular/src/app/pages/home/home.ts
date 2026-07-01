6a320ad8bdfc6960cc06d55c46192e40e85baa35
thecoaty <xkzin_@hotmail.com>
2026-07-01 16:13:02 -0300

    inicio do menu hamburguer

--- sprint_6_angular/sprint_angular/src/app/pages/home/home.ts
+++ sprint_6_angular/sprint_angular/src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -13,5 +15,10 @@ export class Home {
 
   logout():void{
     this.auth.logout()
+  }
+
+  showElement = true;
+  switchElement(){
+    this.showElement = !this.showElement
   }
 }
